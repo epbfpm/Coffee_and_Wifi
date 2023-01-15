6e646cd68d1adf6985feea35db242d4cc937e324
epbfpm <epbfpm@gmail.com>
2023-01-15 19:35:13 -0300

    Initial commit

---
+++ setup.py
@@ -0,0 +1,12 @@
+from setuptools import setup
+
+setup(
+    name='Coffee_and_Wifi',
+    version='',
+    packages=[''],
+    url='',
+    license='',
+    author='elder',
+    author_email='',
+    description=''
+)
